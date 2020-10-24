bf433553208ed74dc52e37b5de80ea3db80eae8d
Esther García <esther.g.g.01@gmail.com>
2020-10-24 19:14:56 +0200

    Inicial

---
+++ main.cpp
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: Esther García Gallego
+ *
+ * Created on 24 de octubre de 2020, 17:59
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
